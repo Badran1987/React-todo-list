941a8a8657851538cdc3f31d7b0f00e4931ce3b8
Mohammed Badran <badran87@gmail.com>
2021-02-22 12:19:00 -0500

    fix return issue

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -5,8 +5,8 @@ import React, { Component } from 'react';
 let TaskItem = ({id, task, complete}) =>{
 	return (
 		<li>
-			<input type="checkbox" id={`2`} name={`2`} checked='fales' />
-			<label for= {`task-${id}`} className="task">badran</label>
+			<input type="checkbox" id={id} name={task} checked={complete} />
+			<label for= {`task-${id}`} className="task">{task}</label>
 		</li>
 	);
 }
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import data from "./fetch.json"; 
+import data from './fetch.json'; 
 import TaskItem from './TaskItem'
 
 let Tasklist = () =>{
 
-  
-    {data.map((todolist, i)=>{
-
-       return (
-         <TaskItem id = {todolist.id} task = {todolist.task} complete = {todolist.complete} />
-        ); 
-          
-    })}
+    return (
+     <>
+        {data.map((todolist, i) => (
+          <TaskItem id = {todolist.id} task = {todolist.task} complete = {todolist.complete} />
+        ))}
+      </>
+    );
 
   } 
 
